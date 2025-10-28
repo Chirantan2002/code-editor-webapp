e6e57787618994cde76380c330b79ac771123a8a
Chirantan <chirantanbiswas2017@gmail.com>
2025-10-29 01:37:38 +0530

    Fixed the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "../styles/DefaultCSS.css";
-import "../styles/DisabledCursor.css";
+import "../styles/disabledCursor.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import ConvexClientProvider from "@/components/providers/ConvexClientProvider";
 import Footer from "@/components/Footer";
